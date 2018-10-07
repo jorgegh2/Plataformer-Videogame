0eb67687748a7f2a6b332c38a3f4eec911a152a7
GerardBP77 <GerardBP77@users.noreply.github.com>
2018-10-07 21:51:11 +0200

    Add Map Level 2
    Adding level 2 map and updating level 1

---
+++ SuperAwesomeGame2/Game/maps/TilesetLevel2/TilesetWinterObjects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="TilesetWinterObjects128x128" tilewidth="128" tileheight="128" tilecount="100" columns="10">
+ <image source="Objects/TilesetWinterObjects128x128.png" trans="ff00ff" width="1280" height="1280"/>
+</tileset>
