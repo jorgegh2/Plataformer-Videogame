0a75e9a3cea7004ab42d02e78f7a8ab016703f03
GerardBP77 <GerardBP77@users.noreply.github.com>
2018-10-08 16:32:06 +0200

    Test Layers Tile MAp

--- SuperAwesomeGame2/Game/maps/TilsetLevel1/BgLevel1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.1" tiledversion="2018.09.12" name="BG" tilewidth="128" tileheight="128" tilecount="35" columns="7">
- <image source="BG/BG.png" trans="ff00ff" width="1000" height="750"/>
-</tileset>
