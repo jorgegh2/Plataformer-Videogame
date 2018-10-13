65f506e24e5c4952844c721c0f7994653a8a3553
GerardBP77 <GerardBP77@users.noreply.github.com>
2018-10-14 01:41:43 +0200

    Add dead transition (partly)
    Need to fix it  but the conditions are working

--- SuperAwesomeGame2/Motor2D/j1Player.h
+++ SuperAwesomeGame2/Motor2D/j1Player.h
@@ -12,7 +12,7 @@ struct Mix_Chunk;
 struct Collider;
 
 
-enum state {NONE, JUMP, ONFLOOR, ONAIR, LANDING};
+enum state {NONE, JUMP, ONFLOOR, ONAIR, LANDING, DEAD};
 
 class j1Player : public j1Module
 {
